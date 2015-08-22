46a4b9fb914a9e04333b6d9f1e2fa39e16f8e9ee
Yasha <yasha@iagent.ru>
2015-08-22 17:41:03 +0300

    app.js
    add applicants

--- build/js/app.js
+++ build/js/app.js
@@ -30,9 +30,6 @@ angular.module('thalamusApp', [
       	when('/documents/list', {
         templateUrl: 'tmpl/doc_list.html'
       }).
-      	when('/calendar_apl', {
-        templateUrl: 'tmpl/calendar_apl.html'
-      }).
 	when('/administration', {
         templateUrl: 'tmpl/security.html'
       }).
