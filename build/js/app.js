5627b4211496f5aa903e0bef29885d535a4a2f25
t.zaitseva <tanya@notamedia.ru>
2015-09-08 13:41:41 +0300

    add aplicants tiles and statistic

--- build/js/app.js
+++ build/js/app.js
@@ -43,6 +43,9 @@ angular.module('thalamusApp', [
 	when('/applicants_list', {
         templateUrl: 'tmpl/applicants_list.html'
       }).
+	when('/applicants_tiles', {
+        templateUrl: 'tmpl/applicants_tiles.html'
+      }).
 	when('/program', {
         templateUrl: 'tmpl/program.html'
       }).
